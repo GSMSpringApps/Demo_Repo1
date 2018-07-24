b560d7fe74e1c3a17fe24ed2b4319e5077315c67
GSMSpringApps <gsmohanty88@gmail.com>
2018-07-24 21:27:53 +0530

    Settings Saved

---
+++ src/main/java/com/gsm/spring/SpringBootAppGithubDemo1Application.java
@@ -0,0 +1,12 @@
+package com.gsm.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppGithubDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppGithubDemo1Application.class, args);
+	}
+}
---
+++ src/test/java/com/gsm/spring/SpringBootAppGithubDemo1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gsm.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAppGithubDemo1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
