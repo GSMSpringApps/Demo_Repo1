6b6d5f6ea004d2f9780df4d4a06523c0141a5049
GSMSpringApps <gsmohanty88@gmail.com>
2018-07-24 21:33:54 +0530

    Annotation @Id created

--- src/main/java/com/gsm/spring/model/User.java
+++ src/main/java/com/gsm/spring/model/User.java
@@ -1,10 +1,11 @@
 package com.gsm.spring.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class User {
-
+    @Id
 	private String name;
 
 	public String getName() {
